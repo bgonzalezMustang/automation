58759b956c0f6d4f1385057ff56968c5d1d71d49
bgonzalezMustang <89812126+bgonzalezMustang@users.noreply.github.com>
2021-11-03 15:36:42 -0500

    Add files via upload
    added log verification and delay based on log for several functions. all changed portions of cade marked with "modified by bgon"

--- folderFunctions.py
+++ folderFunctions.py
@@ -4,11 +4,12 @@ import winshell
 import pythoncom
 from pathlib import Path
 import shutil
+import time
 
 # refData.py contains all of our desired reference data
 from refData import *
 
-#iterated through all files in a folder, creates text file with number of files called "!LOG_FILE-"
+#iterateS through all files in a folder, creates text file with number of files called "!LOG_FILE-"
 def createLog(cadFiles):
     fileCount = 1
 
@@ -23,7 +24,9 @@ def createLog(cadFiles):
     Path(cadFiles['folderPath']+ "/!LOG_FILE-" + str(fileCount) + ".txt").touch()
     return None
 
-#function for verifyight that the number of files in a folder is equal to the number of files declared by the log file
+#modified by bgon
+#function for verifying that the number of files in a folder is equal to the number of files declared by the log file
+#returns as a boolean, true when there is a log file in the folder and the number of files in the folder is equal to the log count
 def verifyLog(folderPath):
     logCount = 0
     fileCount = 0
@@ -36,7 +39,18 @@ def verifyLog(folderPath):
                    logCount = int(item.split("-")[1].split(".")[0])
     return (logCount > 0 and logCount == fileCount)
 
+#modified by bgon
+def checkLog(folderPath):
+    currentWait=0
+    maxWait=300
+    isLog = False
+    while isLog != True and currentWait<maxWait:
+        time.delay(5)
+        currentWait+=5
+        isLog=verifyLog(folderPath)
+    return isLog
 
+#modified by bgon
 # Given a folder, identify the non-OLD cad pdf (should be the only pdf that contains BRK and does not contain OLD)
 # From the pdf name, identify the builder
 def identifyCAD(folderPath):
@@ -69,29 +83,43 @@ def createOldCopy(cadFiles):
 def parsePDF(folderPath, pdfName):
     return False
 
+#modified by bgon
 def checkScript(cadFiles):
-    passTest = parsePDF(cadFiles['folderPath'], cadFiles['pdfName'])
-    # If pass, move to Ready For Check
-    if passTest:
-        shutil.move(cadFiles['folderPath'],readyToCheckPath+'/DRAWN BY '+cadFiles['author']+'/'+cadFiles['folderName'])
-    # Else, kick back to In Progress
+    if checkLog(cadFiles['folderPath']):
+        passTest = parsePDF(cadFiles['folderPath'], cadFiles['pdfName'])
+        # If pass, move to Ready For Check
+        if passTest:
+            shutil.move(cadFiles['folderPath'],readyToCheckPath+'/DRAWN BY '+cadFiles['author']+'/'+cadFiles['folderName'])
+        # Else, kick back to In Progress
+        else:
+            shutil.move(cadFiles['folderPath'],cadInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
     else:
-        shutil.move(cadFiles['folderPath'],cadInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
+        Path(cadFiles['folderPath']+"/!SYNCING_ERROR.txt").touch()
     return None
 
+#modified by bgon
 def moveToFinished(cadFiles):
     # If builder on list, move to FINISHED FROM CAD in the respective builder file
-    if cadFiles['builder'] in builderPaths.keys():
-        shutil.move(cadFiles['folderPath'],publicDownloadsPath+'/'+cadFiles['folderName'])
-        shutil.move(publicDownloadsPath+'/'+cadFiles['folderName'],finishedFromCADPath+'/'+builderPaths[cadFiles['builder']]['FFC']+'/'+cadFiles['folderName'])
-    # If builder not on list (likely PEBKAC), move back to NEEDS CORRECTIONS w/ note
+    if checkLog(cadFiles['folderPath']):
+        if (cadFiles['builder'] in builderPaths.keys()):
+            shutil.move(cadFiles['folderPath'],publicDownloadsPath+'/'+cadFiles['folderName'])
+            shutil.move(publicDownloadsPath+'/'+cadFiles['folderName'],finishedFromCADPath+'/'+builderPaths[cadFiles['builder']]['FFC']+'/'+cadFiles['folderName'])
+        # If builder not on list (likely PEBKAC), move back to NEEDS CORRECTIONS w/ note
+        else:
+            shutil.move(cadFiles['folderPath'],checkInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
+            Path(checkInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName']+'/'+'ERROR - BUILDER NAME NOT FOUND.txt').touch()
     else:
-        shutil.move(cadFiles['folderPath'],checkInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
-        Path(checkInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName']+'/'+'ERROR - BUILDER NAME NOT FOUND.txt').touch()
+        Path(cadFiles['folderPath']+"/!SYNCING_ERROR.txt").touch()
     return None
 
+
+#modified by bgon
 def moveToInProgress(cadFiles):
-    shutil.move(cadFiles['folderPath'],cadInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
+    #Evaluating whether or not the number of files is correct and there is a log file before it is moved
+    if checkLog(cadFiles['folderPath']):   
+        shutil.move(cadFiles['folderPath'],cadInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
+    else:
+        Path(cadFiles['folderPath']+"/!SYNCING_ERROR.txt").touch()
     return None
 
 # searches builder directory for dwg with same name as pdf
