45d92fc0b7aaacbafc310195f3e4872bafd28bf8
bgonzalezMustang <89812126+bgonzalezMustang@users.noreply.github.com>
2022-04-25 13:30:50 -0500

    Most recent versions of FileManagement/TeamsBot

--- folderFunctions.py
+++ folderFunctions.py
@@ -8,49 +8,8 @@ import time
 
 # refData.py contains all of our desired reference data
 from refData import *
+from checkPDF import *
 
-#iterateS through all files in a folder, creates text file with number of files called "!LOG_FILE-"
-def createLog(cadFiles):
-    fileCount = 1
-
-    for folder in os.walk(cadFiles['folderPath']):
-        if folder[0] == cadFiles['folderPath']:
-            for item in folder[2]:
-                if "!LOG_FILE" in item:
-                    os.remove(cadFiles['folderPath']+ "/" + item)
-                else:
-                    fileCount +=  1
-            return fileCount
-    Path(cadFiles['folderPath']+ "/!LOG_FILE-" + str(fileCount) + ".txt").touch()
-    return None
-
-#modified by bgon
-#function for verifying that the number of files in a folder is equal to the number of files declared by the log file
-#returns as a boolean, true when there is a log file in the folder and the number of files in the folder is equal to the log count
-def verifyLog(folderPath):
-    logCount = 0
-    fileCount = 0
-    for folder in os.walk(folderPath):
-        if folder[0] == folderPath:
-            for item in folder[2]:
-                fileCount += 1
-                if "!LOG_FILE" in item:
-                    # !LOG_FILE-4.txt -> 4.txt -> 4
-                   logCount = int(item.split("-")[1].split(".")[0])
-    return (logCount > 0 and logCount == fileCount)
-
-#modified by bgon
-def checkLog(folderPath):
-    currentWait=0
-    maxWait=300
-    isLog = False
-    while isLog != True and currentWait<maxWait:
-        time.delay(5)
-        currentWait+=5
-        isLog=verifyLog(folderPath)
-    return isLog
-
-#modified by bgon
 # Given a folder, identify the non-OLD cad pdf (should be the only pdf that contains BRK and does not contain OLD)
 # From the pdf name, identify the builder
 def identifyCAD(folderPath):
@@ -80,46 +39,36 @@ def createOldCopy(cadFiles):
                     count += 1
     shutil.copy(cadFiles['folderPath']+'/'+cadFiles['pdfName']+'.pdf', cadFiles['folderPath']+'/OLD'+str(count)+'-'+cadFiles['pdfName']+'.pdf')
 
-def parsePDF(folderPath, pdfName):
-    return False
 
-#modified by bgon
 def checkScript(cadFiles):
-    if checkLog(cadFiles['folderPath']):
-        passTest = parsePDF(cadFiles['folderPath'], cadFiles['pdfName'])
-        # If pass, move to Ready For Check
-        if passTest:
-            shutil.move(cadFiles['folderPath'],readyToCheckPath+'/DRAWN BY '+cadFiles['author']+'/'+cadFiles['folderName'])
-        # Else, kick back to In Progress
-        else:
-            shutil.move(cadFiles['folderPath'],cadInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
+    pdfChecker = PDFChecker()
+    passTest = pdfChecker.checkPDF(cadFiles['folderPath'] + '/' + cadFiles['pdfName'] + '.pdf')
+    print(passTest)
+    # If pass, move to Ready For Check
+    if passTest:
+        shutil.move(cadFiles['folderPath'],readyToCheckPath+'/DRAWN BY '+cadFiles['author']+'/'+cadFiles['folderName'])
+        print('moved to '+readyToCheckPath+'/DRAWN BY '+cadFiles['author']+'/'+cadFiles['folderName'] + '\n')
+    # Else, kick back to In Progress
     else:
-        Path(cadFiles['folderPath']+"/!SYNCING_ERROR.txt").touch()
+        createShortcut(cadFiles)
+        shutil.move(cadFiles['folderPath'],cadInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
+        print('moved to '+cadInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'] + '\n')
     return None
 
-#modified by bgon
 def moveToFinished(cadFiles):
     # If builder on list, move to FINISHED FROM CAD in the respective builder file
-    if checkLog(cadFiles['folderPath']):
-        if (cadFiles['builder'] in builderPaths.keys()):
-            shutil.move(cadFiles['folderPath'],publicDownloadsPath+'/'+cadFiles['folderName'])
-            shutil.move(publicDownloadsPath+'/'+cadFiles['folderName'],finishedFromCADPath+'/'+builderPaths[cadFiles['builder']]['FFC']+'/'+cadFiles['folderName'])
-        # If builder not on list (likely PEBKAC), move back to NEEDS CORRECTIONS w/ note
-        else:
-            shutil.move(cadFiles['folderPath'],checkInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
-            Path(checkInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName']+'/'+'ERROR - BUILDER NAME NOT FOUND.txt').touch()
+    if cadFiles['builder'] in builderPaths.keys():
+        shutil.move(cadFiles['folderPath'],publicDownloadsPath+'/'+cadFiles['folderName'])
+        time.sleep(15)
+        shutil.move(publicDownloadsPath+'/'+cadFiles['folderName'],finishedFromCADPath+'/'+builderPaths[cadFiles['builder']]['FFC']+'/'+cadFiles['folderName'])
+    # If builder not on list (likely PEBKAC), move back to NEEDS CORRECTIONS w/ note
     else:
-        Path(cadFiles['folderPath']+"/!SYNCING_ERROR.txt").touch()
+        shutil.move(cadFiles['folderPath'],checkInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
+        Path(checkInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName']+'/'+'!ERROR - BUILDER NAME NOT FOUND.txt').touch()
     return None
 
-
-#modified by bgon
 def moveToInProgress(cadFiles):
-    #Evaluating whether or not the number of files is correct and there is a log file before it is moved
-    if checkLog(cadFiles['folderPath']):   
-        shutil.move(cadFiles['folderPath'],cadInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
-    else:
-        Path(cadFiles['folderPath']+"/!SYNCING_ERROR.txt").touch()
+    shutil.move(cadFiles['folderPath'],cadInProgressPath+'/'+cadFiles['author']+'/'+cadFiles['folderName'])
     return None
 
 # searches builder directory for dwg with same name as pdf
@@ -132,7 +81,7 @@ def createShortcut(cadFiles):
                     return None
     # Sets target to none, then searches the builder path for a DWG with the PDF's name
     target = None
-    if not(cadFiles['builder'] is None):
+    if cadFiles['builder'] is builderPaths.keys():
         for folder in os.walk(cadDWGsPath + '/' + builderPaths[cadFiles['builder']]['DWG']):
             if target != None:
                 break
@@ -140,6 +89,15 @@ def createShortcut(cadFiles):
                 if item != None and cadFiles['pdfName'] in item and '.dwg' in item:
                     target = folder[0] + '/' + item
                     break
+    else:
+        for folder in os.walk(cadDWGsPath):
+            if target != None:
+                break
+            for item in folder[2]:
+                if item != None and cadFiles['pdfName'] in item and '.dwg' in item:
+                    target = folder[0] + '/' + item
+                    break
+
     # If a target is found, make a shortcut
     if target != None:
         pythoncom.CoInitialize()
