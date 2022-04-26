5c9d7edb341731e7042bf55b75040133a863020f
bgonzalezMustang <89812126+bgonzalezMustang@users.noreply.github.com>
2022-04-26 12:04:28 -0500

    Launches Teams bot and file management
    Added startup event in task scheduler, should be always working

---
+++ MyStartup.bat
@@ -0,0 +1,3 @@
+start /B python "C:\Users\bgonzalez\OneDrive - Mustang Plumbing\Documents\Github\automation\cadFileManagement\main.py" 
+start /B python "C:\Users\bgonzalez\OneDrive - Mustang Plumbing\Documents\Github\automation\TeamsBot\main.py"
+
