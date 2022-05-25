7a6f5ee4c7a43400d890c0ed1bf8e6d3d97dad0f
bgonzalezMustang <89812126+bgonzalezMustang@users.noreply.github.com>
2022-05-25 10:01:52 -0500

    Update MyStartup.bat

--- cadFileManagement/MyStartup.bat
+++ cadFileManagement/MyStartup.bat
@@ -1,5 +1,3 @@
-powershell if((get-process -name "Python") -eq $Null){echo "No active python instance"} else{stop-process -name "Python"}
-
 "C:\Users\bgonzalez\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\bgonzalez\OneDrive - Mustang Plumbing\Documents\Github\automation\TeamsBot\main.py"
 pause
 
